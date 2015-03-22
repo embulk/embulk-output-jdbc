171e54b02bdb0b6cd0016bd965df4cdb71650cd9
hito4t <thitoshi@cac.co.jp>
2015-03-22 15:52:57 +0900

    TimestampFormat

--- embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/ColumnDefinition.java
+++ embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/ColumnDefinition.java
@@ -2,20 +2,25 @@ package org.embulk.output.oracle.oci;
 
 public class ColumnDefinition
 {
-
     public static int SQLT_CHR = 1;
     public static int SQLT_INT = 3;
 
     public final String columnName;
     public final int columnType;
     public final int columnSize;
+    public final String dateFormat;
 
 
-    public ColumnDefinition(String columnName, int columnType, int columnSize)
+    public ColumnDefinition(String columnName, int columnType, int columnSize, String dateFormat)
     {
         this.columnName = columnName;
         this.columnType = columnType;
         this.columnSize = columnSize;
+        this.dateFormat = dateFormat;
     }
 
+    public ColumnDefinition(String columnName, int columnType, int columnSize)
+    {
+        this(columnName, columnType, columnSize, null);
+    }
 }
