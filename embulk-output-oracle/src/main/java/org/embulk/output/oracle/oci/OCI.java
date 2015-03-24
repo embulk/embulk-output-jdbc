527059062afd4eb75d77a3dcfbb50e0c426b6bbd
hito4t <thitoshi@cac.co.jp>
2015-03-24 14:31:09 +0900

    search library automatically

--- embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/OCI.java
+++ embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/OCI.java
@@ -1,10 +1,19 @@
 package org.embulk.output.oracle.oci;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 
 public class OCI
 {
     static {
-        System.loadLibrary("embulk-oracle");
+        try {
+            loadLibrary();
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public native byte[] createContext();
@@ -23,4 +32,48 @@ public class OCI
 
     public native void close(byte[] context);
 
+    private static void loadLibrary() throws MalformedURLException, URISyntaxException {
+        URL url = OCI.class.getResource("/" + OCI.class.getName().replace('.', '/') + ".class");
+        if (url.toString().startsWith("jar:")) {
+            url = new URL(url.toString().replaceAll("^jar:", "").replaceAll("![^!]*$", ""));
+        }
+
+        File folder = new File(url.toURI()).getParentFile();
+        for (;; folder = folder.getParentFile()) {
+            if (folder == null) {
+                throw new RuntimeException("embulk-oracle library not found.");
+            }
+
+            if (folder.getName().startsWith("embulk-output-oracle")) {
+                break;
+            }
+        }
+
+
+
+
+        System.out.println(folder);
+
+        if (!loadLibrary(folder)) {
+            throw new RuntimeException("embulk-oracle library not found.");
+        }
+    }
+
+    private static boolean loadLibrary(File folder) {
+        String libraryName = System.mapLibraryName("embulk-oracle");
+        for (File child : folder.listFiles()) {
+            if (child.isFile()) {
+                if (child.getName().equals(libraryName)) {
+                    System.load(child.getAbsolutePath());
+                    return true;
+                }
+            }
+            if (child.isDirectory()) {
+                if (loadLibrary(child)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
