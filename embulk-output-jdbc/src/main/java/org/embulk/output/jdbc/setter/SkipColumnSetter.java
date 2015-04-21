fb61e7c9f02d17871a4b5a2069c6c289ab1c5f64
hito4t <thitoshi@cac.co.jp>
2015-04-21 19:03:30 +0900

    Remove unused imports

--- embulk-output-jdbc/src/main/java/org/embulk/output/jdbc/setter/SkipColumnSetter.java
+++ embulk-output-jdbc/src/main/java/org/embulk/output/jdbc/setter/SkipColumnSetter.java
@@ -1,11 +1,7 @@
 package org.embulk.output.jdbc.setter;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 import org.embulk.spi.PageReader;
 import org.embulk.spi.time.Timestamp;
-import org.embulk.output.jdbc.JdbcColumn;
 import org.embulk.output.jdbc.BatchInsert;
 
 public class SkipColumnSetter
