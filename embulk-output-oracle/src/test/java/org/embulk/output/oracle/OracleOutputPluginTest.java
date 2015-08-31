01ee06a01fc8702bee053b883a1ca123ec8dd91d
hito4t <thitoshi@cac.co.jp>
2015-08-31 13:52:04 +0900

    Use EmbulkEmbed for test

--- embulk-output-oracle/src/test/java/org/embulk/output/oracle/OracleOutputPluginTest.java
+++ embulk-output-oracle/src/test/java/org/embulk/output/oracle/OracleOutputPluginTest.java
@@ -4,8 +4,14 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.embulk.input.filesplit.LocalFileSplitInputPlugin;
+import org.embulk.output.tester.EmbulkPluginTester;
+import org.embulk.plugin.PluginClassLoader;
+import org.embulk.spi.InputPlugin;
+import org.embulk.spi.OutputPlugin;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -26,6 +32,8 @@ public class OracleOutputPluginTest
     private static List<NamedObject> testObjects = new ArrayList<NamedObject>();
     private static NamedObject test12c;
     private static NamedObject test11g;
+    private static EmbulkPluginTester tester = new EmbulkPluginTester();
+
 
     @BeforeClass
     public static void beforeClass() throws Exception
@@ -35,14 +43,16 @@ public class OracleOutputPluginTest
             System.setProperty("file.encoding", "MS932");
         }
 
-        test12c = createTest("/driver/12c/ojdbc7.jar");
+        tester.addPlugin(InputPlugin.class, "filesplit", LocalFileSplitInputPlugin.class);
+
+        test12c = createTest("oracle12", "/driver/12c/ojdbc7.jar");
         if (test12c == null) {
             System.out.println("Warning: you should put ojdbc7.jar (version 12c) on 'test/resources/driver/12c' directory.");
         } else {
             testObjects.add(test12c);
         }
 
-        test11g = createTest("/driver/11g/ojdbc6.jar");
+        test11g = createTest("oracle11", "/driver/11g/ojdbc6.jar");
         if (test11g == null) {
             System.out.println("Warning: you should put ojdbc6.jar (version 11g Release 2) on 'test/resources/driver/11g' directory.");
         } else {
@@ -50,25 +60,37 @@ public class OracleOutputPluginTest
         }
     }
 
-    private static NamedObject createTest(String jdbcDriverPath) throws Exception
+    private static NamedObject createTest(String pluginName, String jdbcDriverPath) throws Exception
     {
-        String[] classPaths = System.getProperty("java.class.path").split(System.getProperty("path.separator"));
+        // Load OracleOutputPluginTestImpl, Oracle JDBC Driver, embulk-output-oracle by another ClassLoader
+        // in order to test for different driver versions.
+
         List<URL> urls = new ArrayList<URL>();
-        for (String classPath : classPaths) {
-            urls.add(new File(classPath).toURI().toURL());
-        }
-        // load Oracle JDBC driver dynamically, in order to enable to build without the driver.
-        URL url = OracleOutputPluginTest.class.getResource(jdbcDriverPath);
-        if (url == null) {
+
+        File testRoot = new File(OracleOutputPluginTest.class.getResource("/dummy.txt").toURI()).getParentFile();
+        String pluginClassName = "org.embulk.output.OracleOutputPlugin";
+        URL pluginClassUrl = OracleOutputPluginTest.class.getResource("/"  +pluginClassName.replace('.', '/') + ".class");
+        File root = new File(pluginClassUrl.toURI()).getParentFile().getParentFile().getParentFile().getParentFile();
+
+        urls.add(root.toURI().toURL());
+        urls.add(testRoot.toURI().toURL());
+
+        URL jdbcDriverUrl = OracleOutputPluginTest.class.getResource(jdbcDriverPath);
+        if (jdbcDriverUrl == null) {
             return null;
         }
 
-        urls.add(url);
-        ClassLoader classLoader = new ChildFirstClassLoader(urls, OracleOutputPluginTest.class.getClassLoader());
+        urls.add(jdbcDriverUrl);
+        ClassLoader classLoader = new PluginClassLoader(urls, OracleOutputPluginTest.class.getClassLoader(),
+                Arrays.asList(EmbulkPluginTester.class.getPackage().getName()), new ArrayList<String>());
         Thread.currentThread().setContextClassLoader(classLoader);
+        tester.addPlugin(OutputPlugin.class, pluginName, classLoader.loadClass(pluginClassName));
 
         Class<?> testClass = classLoader.loadClass(OracleOutputPluginTest.class.getName() + "Impl");
         final Object testObject = testClass.newInstance();
+        invoke(testObject, "setTester", tester);
+        invoke(testObject, "setPluginName", pluginName);
+
         final String version = (String)invoke(testObject, "beforeClass");
         if (version == null) {
             return null;
@@ -78,7 +100,6 @@ public class OracleOutputPluginTest
 
     private static void invoke(String methodName) throws Exception
     {
-        //invoke(test12c, methodName);
         for (NamedObject testObject : testObjects) {
             invoke(testObject, methodName);
         }
@@ -93,12 +114,16 @@ public class OracleOutputPluginTest
         return null;
     }
 
-    private static Object invoke(Object testObject, String methodName) throws Exception
+    private static Object invoke(Object testObject, String methodName, Object... arguments) throws Exception
     {
         if (testObject != null) {
             Thread.currentThread().setContextClassLoader(testObject.getClass().getClassLoader());
-            Method method = testObject.getClass().getMethod(methodName);
-            return method.invoke(testObject);
+            Class<?>[] parameterTypes = new Class<?>[arguments.length];
+            for (int i = 0; i < arguments.length; i++) {
+                parameterTypes[i] = arguments[i].getClass();
+            }
+            Method method = testObject.getClass().getMethod(methodName, parameterTypes);
+            return method.invoke(testObject, arguments);
         }
         return null;
     }
