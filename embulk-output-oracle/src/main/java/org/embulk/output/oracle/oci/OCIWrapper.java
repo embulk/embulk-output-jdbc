4c11a8bfa04eb366642ca58932f53b44165ff8f9
hito4t <thitoshi@cac.co.jp>
2016-01-06 19:19:53 +0900

    format code

--- embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/OCIWrapper.java
+++ embulk-output-oracle/src/main/java/org/embulk/output/oracle/oci/OCIWrapper.java
@@ -68,29 +68,57 @@ public class OCIWrapper
     public void open(String dbName, String userName, String password) throws SQLException
     {
         Pointer envHandlePointer = createPointerPointer();
-        check("OCIEnvCreate", oci.OCIEnvCreate(envHandlePointer, OCI.OCI_THREADED | OCI.OCI_OBJECT, null, null, null, null, 0, null));
+        check("OCIEnvCreate", oci.OCIEnvCreate(
+                envHandlePointer,
+                OCI.OCI_THREADED | OCI.OCI_OBJECT,
+                null,
+                null,
+                null,
+                null,
+                0,
+                null));
         envHandle = envHandlePointer.getPointer(0);
 
         // error handle
         Pointer errHandlePointer = createPointerPointer();
-        check("OCIHandleAlloc(OCI_HTYPE_ERROR)", oci.OCIHandleAlloc(envHandle,
-                errHandlePointer, OCI.OCI_HTYPE_ERROR, 0, null));
+        check("OCIHandleAlloc(OCI_HTYPE_ERROR)", oci.OCIHandleAlloc(
+                envHandle,
+                errHandlePointer,
+                OCI.OCI_HTYPE_ERROR,
+                0,
+                null));
         errHandle = errHandlePointer.getPointer(0);
 
         // service context
         svcHandlePointer = createPointerPointer();
-        check("OCIHandleAlloc(OCI_HTYPE_SVCCTX)", oci.OCIHandleAlloc(envHandle,
-                svcHandlePointer, OCI.OCI_HTYPE_SVCCTX, 0, null));
+        check("OCIHandleAlloc(OCI_HTYPE_SVCCTX)", oci.OCIHandleAlloc(
+                envHandle,
+                svcHandlePointer,
+                OCI.OCI_HTYPE_SVCCTX,
+                0,
+                null));
 
         // logon
         // dbName should be defined in 'tnsnames.ora' or a form of "host:port/db"
-        check("OCILogon", oci.OCILogon(envHandle, errHandle, svcHandlePointer,
-                userName, userName.length(), password, password.length(), dbName, dbName.length()));
+        check("OCILogon", oci.OCILogon(
+                envHandle,
+                errHandle,
+                svcHandlePointer,
+                userName,
+                userName.length(),
+                password,
+                password.length(),
+                dbName,
+                dbName.length()));
         svcHandle = svcHandlePointer.getPointer(0);
 
         Pointer dpHandlePointer = createPointerPointer();
-        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_CTX)", oci.OCIHandleAlloc(envHandle,
-                dpHandlePointer, OCI.OCI_HTYPE_DIRPATH_CTX, 0, null));
+        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_CTX)", oci.OCIHandleAlloc(
+                envHandle,
+                dpHandlePointer,
+                OCI.OCI_HTYPE_DIRPATH_CTX,
+                0,
+                null));
         dpHandle = dpHandlePointer.getPointer(0);
     }
 
@@ -100,69 +128,131 @@ public class OCIWrapper
 
         // load table name
         Pointer tableName = createPointer(tableDefinition.getTableName());
-        check("OCIAttrSet(OCI_ATTR_NAME)", oci.OCIAttrSet(dpHandle, OCI.OCI_HTYPE_DIRPATH_CTX,
-                tableName, (int)tableName.size(), OCI.OCI_ATTR_NAME, errHandle));
+        check("OCIAttrSet(OCI_ATTR_NAME)", oci.OCIAttrSet(
+                dpHandle,
+                OCI.OCI_HTYPE_DIRPATH_CTX,
+                tableName,
+                (int)tableName.size()
+                , OCI.OCI_ATTR_NAME,
+                errHandle));
 
         Pointer cols = createPointer((short)tableDefinition.getColumnCount());
-        check("OCIAttrSet(OCI_ATTR_NUM_COLS)", oci.OCIAttrSet(dpHandle, OCI.OCI_HTYPE_DIRPATH_CTX,
-                cols, (int)cols.size(), OCI.OCI_ATTR_NUM_COLS, errHandle));
+        check("OCIAttrSet(OCI_ATTR_NUM_COLS)", oci.OCIAttrSet(
+                dpHandle,
+                OCI.OCI_HTYPE_DIRPATH_CTX,
+                cols,
+                (int)cols.size(),
+                OCI.OCI_ATTR_NUM_COLS,
+                errHandle));
 
         Pointer columnsPointer = createPointerPointer();
-        check("OCIAttrGet(OCI_ATTR_LIST_COLUMNS)", oci.OCIAttrGet(dpHandle, OCI.OCI_HTYPE_DIRPATH_CTX,
-                columnsPointer, null, OCI.OCI_ATTR_LIST_COLUMNS, errHandle));
+        check("OCIAttrGet(OCI_ATTR_LIST_COLUMNS)", oci.OCIAttrGet(
+                dpHandle,
+                OCI.OCI_HTYPE_DIRPATH_CTX,
+                columnsPointer,
+                null,
+                OCI.OCI_ATTR_LIST_COLUMNS,
+                errHandle));
         Pointer columns = columnsPointer.getPointer(0);
 
         for (int i = 0; i < tableDefinition.getColumnCount(); i++) {
             ColumnDefinition columnDefinition = tableDefinition.getColumn(i);
 
             Pointer columnPointer = createPointerPointer();
-            check("OCIParamGet(OCI_DTYPE_PARAM)", oci.OCIParamGet(columns, OCI.OCI_DTYPE_PARAM, errHandle,
-                    columnPointer, i + 1));
+            check("OCIParamGet(OCI_DTYPE_PARAM)", oci.OCIParamGet(
+                    columns,
+                    OCI.OCI_DTYPE_PARAM,
+                    errHandle,
+                    columnPointer,
+                    i + 1));
             Pointer column = columnPointer.getPointer(0);
 
             Pointer columnName = createPointer(columnDefinition.getColumnName());
-            check("OCIAttrSet(OCI_ATTR_NAME)", oci.OCIAttrSet(column, OCI.OCI_DTYPE_PARAM,
-                    columnName, (int)columnName.size(), OCI.OCI_ATTR_NAME, errHandle));
+            check("OCIAttrSet(OCI_ATTR_NAME)", oci.OCIAttrSet(
+                    column,
+                    OCI.OCI_DTYPE_PARAM,
+                    columnName,
+                    (int)columnName.size(),
+                    OCI.OCI_ATTR_NAME,
+                    errHandle));
 
             Pointer dataType = createPointer(columnDefinition.getDataType());
-            check("OCIAttrSet(OCI_ATTR_DATA_TYPE)", oci.OCIAttrSet(column, OCI.OCI_DTYPE_PARAM,
-                    dataType, (int)dataType.size(), OCI.OCI_ATTR_DATA_TYPE, errHandle));
+            check("OCIAttrSet(OCI_ATTR_DATA_TYPE)", oci.OCIAttrSet(
+                    column,
+                    OCI.OCI_DTYPE_PARAM,
+                    dataType,
+                    (int)dataType.size(),
+                    OCI.OCI_ATTR_DATA_TYPE,
+                    errHandle));
 
             Pointer dataSize = createPointer(columnDefinition.getDataSize());
-            check("OCIAttrSet(OCI_ATTR_DATA_SIZE)", oci.OCIAttrSet(column, OCI.OCI_DTYPE_PARAM,
-                    dataSize, (int)dataSize.size(), OCI.OCI_ATTR_DATA_SIZE, errHandle));
+            check("OCIAttrSet(OCI_ATTR_DATA_SIZE)", oci.OCIAttrSet(
+                    column,
+                    OCI.OCI_DTYPE_PARAM,
+                    dataSize,
+                    (int)dataSize.size(),
+                    OCI.OCI_ATTR_DATA_SIZE,
+                    errHandle));
 
             // need to set charset explicitly because database charset is not set by default.
             Pointer charsetId = createPointer(columnDefinition.getCharset().getId());
-            check("OCIAttrSet(OCI_ATTR_CHARSET_ID)", oci.OCIAttrSet(column, OCI.OCI_DTYPE_PARAM,
-                    charsetId, (int)charsetId.size(), OCI.OCI_ATTR_CHARSET_ID, errHandle));
+            check("OCIAttrSet(OCI_ATTR_CHARSET_ID)", oci.OCIAttrSet(
+                    column,
+                    OCI.OCI_DTYPE_PARAM,
+                    charsetId,
+                    (int)charsetId.size(),
+                    OCI.OCI_ATTR_CHARSET_ID,
+                    errHandle));
 
             if (columnDefinition.getDateFormat() != null) {
                 Pointer dateFormat = createPointer(columnDefinition.getDateFormat());
-                check("OCIAttrSet(OCI_ATTR_DATEFORMAT)", oci.OCIAttrSet(column, OCI.OCI_DTYPE_PARAM,
-                        dateFormat, (int)dateFormat.size(), OCI.OCI_ATTR_DATEFORMAT, errHandle));
+                check("OCIAttrSet(OCI_ATTR_DATEFORMAT)", oci.OCIAttrSet(
+                        column,
+                        OCI.OCI_DTYPE_PARAM,
+                        dateFormat,
+                        (int)dateFormat.size(),
+                        OCI.OCI_ATTR_DATEFORMAT,
+                        errHandle));
             }
 
-            check("OCIDescriptorFree(OCI_DTYPE_PARAM)", oci.OCIDescriptorFree(column, OCI.OCI_DTYPE_PARAM));
+            check("OCIDescriptorFree(OCI_DTYPE_PARAM)", oci.OCIDescriptorFree(
+                    column,
+                    OCI.OCI_DTYPE_PARAM));
         }
 
-        check("OCIDirPathPrepare", oci.OCIDirPathPrepare(dpHandle, svcHandle, errHandle));
+        check("OCIDirPathPrepare", oci.OCIDirPathPrepare(
+                dpHandle,
+                svcHandle,
+                errHandle));
 
         // direct path column array
         Pointer dpcaHandlePointer = createPointerPointer();
-        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_COLUMN_ARRAY)", oci.OCIHandleAlloc(dpHandle,
-                dpcaHandlePointer, OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY, 0, null));
+        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_COLUMN_ARRAY)", oci.OCIHandleAlloc(
+                dpHandle,
+                dpcaHandlePointer,
+                OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY,
+                0,
+                null));
         dpcaHandle = dpcaHandlePointer.getPointer(0);
 
         Pointer dpstrHandlePointer = createPointerPointer();
-        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_STREAM)", oci.OCIHandleAlloc(dpHandle,
-                dpstrHandlePointer, OCI.OCI_HTYPE_DIRPATH_STREAM, 0, null));
+        check("OCIHandleAlloc(OCI_HTYPE_DIRPATH_STREAM)", oci.OCIHandleAlloc(
+                dpHandle,
+                dpstrHandlePointer,
+                OCI.OCI_HTYPE_DIRPATH_STREAM,
+                0,
+                null));
         dpstrHandle = dpstrHandlePointer.getPointer(0);
 
         Pointer maxRowCountPointer = createPointer(0);
 
-        check("OCIAttrGet(OCI_ATTR_NUM_ROWS)", oci.OCIAttrGet(dpcaHandle, OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY,
-                maxRowCountPointer, null, OCI.OCI_ATTR_NUM_ROWS, errHandle));
+        check("OCIAttrGet(OCI_ATTR_NUM_ROWS)", oci.OCIAttrGet(
+                dpcaHandle,
+                OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY,
+                maxRowCountPointer,
+                null,
+                OCI.OCI_ATTR_NUM_ROWS,
+                errHandle));
         maxRowCount = maxRowCountPointer.getInt(0);
     }
 
@@ -178,8 +268,14 @@ public class OCIWrapper
             for (short col = 0; col < tableDefinition.getColumnCount(); col++) {
                 short size = sizes[i++];
 
-                check("OCIDirPathColArrayEntrySet", oci.OCIDirPathColArrayEntrySet(dpcaHandle, errHandle,
-                        rowCount, col, new BoundedMemoryIO(pointer, position, size), size, OCI.OCI_DIRPATH_COL_COMPLETE));
+                check("OCIDirPathColArrayEntrySet", oci.OCIDirPathColArrayEntrySet(
+                        dpcaHandle,
+                        errHandle,
+                        rowCount,
+                        col,
+                        new BoundedMemoryIO(pointer, position, size),
+                        size,
+                        OCI.OCI_DIRPATH_COL_COMPLETE));
 
                 position += size;
             }
@@ -199,21 +295,37 @@ public class OCIWrapper
     private void loadRows(int rowCount) throws SQLException
     {
         for (int offset = 0; offset < rowCount;) {
-            check("OCIDirPathStreamReset", oci.OCIDirPathStreamReset(dpstrHandle, errHandle));
+            check("OCIDirPathStreamReset", oci.OCIDirPathStreamReset(
+                    dpstrHandle,
+                    errHandle));
 
-            short result = oci.OCIDirPathColArrayToStream(dpcaHandle, dpHandle, dpstrHandle, errHandle, rowCount, offset);
+            short result = oci.OCIDirPathColArrayToStream(
+                    dpcaHandle,
+                    dpHandle,
+                    dpstrHandle,
+                    errHandle,
+                    rowCount,
+                    offset);
             if (result != OCI.OCI_SUCCESS && result != OCI.OCI_CONTINUE) {
                 check("OCIDirPathColArrayToStream", result);
             }
 
-            check("OCIDirPathLoadStream", oci.OCIDirPathLoadStream(dpHandle, dpstrHandle, errHandle));
+            check("OCIDirPathLoadStream", oci.OCIDirPathLoadStream(
+                    dpHandle,
+                    dpstrHandle,
+                    errHandle));
 
             if (result == OCI.OCI_SUCCESS) {
                 offset = rowCount;
             } else {
                 Pointer loadedRowCount = createPointer(0);
-                check("OCIAttrGet(OCI_ATTR_ROW_COUNT)", oci.OCIAttrGet(dpcaHandle, OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY,
-                        loadedRowCount, null, OCI.OCI_ATTR_ROW_COUNT, errHandle));
+                check("OCIAttrGet(OCI_ATTR_ROW_COUNT)", oci.OCIAttrGet(
+                        dpcaHandle,
+                        OCI.OCI_HTYPE_DIRPATH_COLUMN_ARRAY,
+                        loadedRowCount,
+                        null,
+                        OCI.OCI_ATTR_ROW_COUNT,
+                        errHandle));
                 offset += loadedRowCount.getInt(0);
             }
         }
@@ -304,7 +416,7 @@ public class OCIWrapper
             try {
                 check("OCIHandleFree", oci.OCIHandleFree(handle, type));
             } catch (SQLException e) {
-                System.err.println(String.format("Warning (OCIHandleFree(%d)) : %s", type, e.getMessage()));
+                logger.warn(String.format("Warning (OCIHandleFree(%d)) : %s", type, e.getMessage()));
             }
         }
     }
