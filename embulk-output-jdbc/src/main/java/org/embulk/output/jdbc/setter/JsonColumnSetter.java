9fa2460178bdeb4cd911f5d946eda58ddf38bdaa
hito4t <thitoshi@cac.co.jp>
2016-04-05 11:40:16 +0900

    Support json

---
+++ embulk-output-jdbc/src/main/java/org/embulk/output/jdbc/setter/JsonColumnSetter.java
@@ -0,0 +1,61 @@
+package org.embulk.output.jdbc.setter;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.embulk.spi.time.Timestamp;
+import org.embulk.output.jdbc.JdbcColumn;
+import org.embulk.output.jdbc.BatchInsert;
+import org.msgpack.value.Value;
+
+public class JsonColumnSetter
+        extends ColumnSetter
+{
+    public JsonColumnSetter(BatchInsert batch, JdbcColumn column,
+            DefaultValueSetter defaultValue)
+    {
+        super(batch, column, defaultValue);
+    }
+
+    @Override
+    public void nullValue() throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void booleanValue(boolean v) throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void longValue(long v) throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void doubleValue(double v) throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void stringValue(String v) throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void timestampValue(Timestamp v) throws IOException, SQLException
+    {
+        defaultValue.setJson();
+    }
+
+    @Override
+    public void jsonValue(Value v) throws IOException, SQLException {
+        batch.setString(v.toJson());
+    }
+}
+
