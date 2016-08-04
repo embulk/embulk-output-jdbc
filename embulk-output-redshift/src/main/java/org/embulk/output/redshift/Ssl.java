6315859de4c34bef94bf325d2c28a62705889374
Antoine Augusti <antoine@drivy.com>
2016-08-04 15:10:34 +0200

    Add support for true or false values

--- embulk-output-redshift/src/main/java/org/embulk/output/redshift/Ssl.java
+++ embulk-output-redshift/src/main/java/org/embulk/output/redshift/Ssl.java
@@ -1,8 +1,26 @@
 package org.embulk.output.redshift;
 
+import java.lang.IllegalArgumentException;
+
 public enum Ssl
 {
     enable,
     disable,
     verify;
+
+    public static Enum fromString(String value)
+    {
+        switch(value) {
+        case "enable":
+        case "true":
+            return enable;
+        case "disable":
+        case "false":
+            return disable;
+        case "verify":
+            return verify;
+        default:
+            throw new IllegalArgumentException("No constant with value " + value + " found. Supported values are: enable, disable, verify.");
+        }
+    }
 }
