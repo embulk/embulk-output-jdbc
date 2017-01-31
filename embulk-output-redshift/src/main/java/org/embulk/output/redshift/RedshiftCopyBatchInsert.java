33522cb760419cc2189174c733221df892e56d2e
takeru911 <takeru_maehara@voyagegroup.com>
2017-01-31 03:01:42 +0000

    import追加

--- embulk-output-redshift/src/main/java/org/embulk/output/redshift/RedshiftCopyBatchInsert.java
+++ embulk-output-redshift/src/main/java/org/embulk/output/redshift/RedshiftCopyBatchInsert.java
@@ -32,6 +32,9 @@ import com.amazonaws.auth.policy.Statement.Effect;
 import com.amazonaws.auth.policy.actions.S3Actions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Region;
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.services.s3.model.PutObjectResult
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient;
 import com.amazonaws.services.securitytoken.model.Credentials;
 import com.amazonaws.services.securitytoken.model.GetFederationTokenRequest;
@@ -225,7 +228,7 @@ public class RedshiftCopyBatchInsert
 
             try {
                 long startTime = System.currentTimeMillis();
-                s3.putObject(createPutObject());
+                PutObjectResult result = s3.putObject(createPutObject());
                 double seconds = (System.currentTimeMillis() - startTime) / 1000.0;
 
                 logger.info(String.format("Uploaded file %s (%.2f seconds)", s3KeyName, seconds));
