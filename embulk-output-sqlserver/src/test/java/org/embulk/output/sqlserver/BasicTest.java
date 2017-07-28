ee2092afbb39e5516174c69f56ea0ee98c53ecd7
hito4t <thitoshi@cac.co.jp>
2017-07-28 16:34:48 +0900

    Refactoring tests

--- embulk-output-sqlserver/src/test/java/org/embulk/output/sqlserver/BasicTest.java
+++ embulk-output-sqlserver/src/test/java/org/embulk/output/sqlserver/BasicTest.java
@@ -62,71 +62,50 @@ public class BasicTest
         //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
     }
 
-    /*
     @Test
-    public void testInsertBeforeLoad() throws Exception
+    public void testInsertCreate() throws Exception
     {
-        execute("insert into test1 values('B001', 0, 'z')");
-        execute("insert into test1 values('B002', 9, 'z')");
+        execute("DROP TABLE TEST1");
 
         Path in1 = toPath("test1.csv");
-        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_insert_before_load.yml")), in1);
-        assertThat(selectRecords(embulk, "test1"), is(readResource("test_insert_before_load_expected.csv")));
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_insert.yml")), in1);
+        assertThat(selectRecords(embulk, "TEST1", in1), is(readResource("test_insert_create_expected.csv")));
         //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
     }
 
     @Test
-    public void testInsertDirectBeforeLoad() throws Exception
+    public void testInsertDirect() throws Exception
     {
-        execute("insert into test1 values('B001', 0, 'z')");
-        execute("insert into test1 values('B002', 9, 'z')");
+        execute("INSERT INTO TEST1(ID) VALUES('9999')");
 
         Path in1 = toPath("test1.csv");
-        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_insert_direct_before_load.yml")), in1);
-        assertThat(selectRecords(embulk, "test1"), is(readResource("test_insert_before_load_expected.csv")));
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_insert_direct.yml")), in1);
+        assertThat(selectRecords(embulk, "TEST1", in1), is(readResource("test_insert_expected.csv")));
         //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
     }
 
     @Test
-    public void testTruncateInsertBeforeLoad() throws Exception
+    public void testInsertDirectCreate() throws Exception
     {
-        execute("insert into test1 values('B001', 0, 'z')");
-        execute("insert into test1 values('B002', 9, 'z')");
+        execute("DROP TABLE TEST1");
 
         Path in1 = toPath("test1.csv");
-        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_truncate_insert_before_load.yml")), in1);
-        assertThat(selectRecords(embulk, "test1"), is(readResource("test_truncate_insert_before_load_expected.csv")));
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_insert_direct.yml")), in1);
+        assertThat(selectRecords(embulk, "TEST1", in1), is(readResource("test_insert_create_expected.csv")));
         //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
     }
 
     @Test
-    public void testMergeBeforeLoad() throws Exception
+    public void testTruncateInsert() throws Exception
     {
-        execute("insert into test1 values('A002', 1, 'y')");
-        execute("insert into test1 values('A003', 1, 'y')");
-        execute("insert into test1 values('B001', 0, 'z')");
-        execute("insert into test1 values('B002', 9, 'z')");
+        execute("INSERT INTO TEST1(ID) VALUES('9999')");
 
         Path in1 = toPath("test1.csv");
-        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_merge_before_load.yml")), in1);
-        assertThat(selectRecords(embulk, "test1"), is(readResource("test_merge_before_load_expected.csv")));
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_truncate_insert.yml")), in1);
+        assertThat(selectRecords(embulk, "TEST1", in1), is(readResource("test_truncate_insert_expected.csv")));
         //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
     }
 
-    @Test
-    public void testMergeDirectBeforeLoad() throws Exception
-    {
-        execute("insert into test1 values('A002', 1, 'y')");
-        execute("insert into test1 values('A003', 1, 'y')");
-        execute("insert into test1 values('B001', 0, 'z')");
-        execute("insert into test1 values('B002', 9, 'z')");
-
-        Path in1 = toPath("test1.csv");
-        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_merge_direct_before_load.yml")), in1);
-        assertThat(selectRecords(embulk, "test1"), is(readResource("test_merge_before_load_expected.csv")));
-        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
-    }
-*/
     private Path toPath(String fileName) throws URISyntaxException
     {
         URL url = Resources.getResource(BASIC_RESOURCE_PATH + fileName);
