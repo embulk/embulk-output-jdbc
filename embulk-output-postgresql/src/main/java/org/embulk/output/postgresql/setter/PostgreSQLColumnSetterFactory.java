b22bb3b345125d58aa6a274d34971ad43c1e2d20
hito4t <thitoshi@cac.co.jp>
2017-10-02 10:45:49 +0900

    Supported INET type of PostgreSQL

--- embulk-output-postgresql/src/main/java/org/embulk/output/postgresql/setter/PostgreSQLColumnSetterFactory.java
+++ embulk-output-postgresql/src/main/java/org/embulk/output/postgresql/setter/PostgreSQLColumnSetterFactory.java
@@ -6,6 +6,7 @@ import org.embulk.output.jdbc.JdbcColumnOption;
 import org.embulk.output.jdbc.setter.ColumnSetter;
 import org.embulk.output.jdbc.setter.ColumnSetterFactory;
 import org.embulk.output.jdbc.setter.JsonColumnSetter;
+import org.embulk.output.jdbc.setter.StringColumnSetter;
 import org.joda.time.DateTimeZone;
 
 public class PostgreSQLColumnSetterFactory
@@ -22,6 +23,8 @@ public class PostgreSQLColumnSetterFactory
         if (column.getSimpleTypeName().equalsIgnoreCase("json") || column.getSimpleTypeName().equalsIgnoreCase("jsonb")) {
             // actually "JSON"/"JSONB"
             return new JsonColumnSetter(batch, column, newDefaultValueSetter(column, option));
+        } else if (column.getSimpleTypeName().equalsIgnoreCase("inet")) {
+            return new StringColumnSetter(batch, column, newDefaultValueSetter(column, option), newTimestampFormatter(option));
         } else {
             return super.newCoalesceColumnSetter(column, option);
         }
